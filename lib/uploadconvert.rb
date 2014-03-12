b81e8a2b92794c88f008bff143d47c4a0a0f0810
M. C. McGrath <shidash@shidash.com>
2014-03-12 12:30:52 -0700

    First version with support for embedded text and image PDFs

---
+++ lib/uploadconvert.rb
@@ -0,0 +1,99 @@
+require 'json'
+require 'docsplit'
+
+class UploadConvert
+
+  def initialize(input)
+    @input = input
+    @output = ""
+    @text = ""
+  end
+
+  # Convert PDFs to JSON
+  def pdfTojson
+    # Extract and clean text
+    @text = detectPDFType
+
+    # Extract metadata and generate output                                                                                          
+    extractMetadataPDF
+    outhash = Hash.new
+    @metadata.each{|k, v| outhash[k] = v}
+    outhash[:text] = @text
+    outhash[:input] = @input
+    @output = JSON.pretty_generate(outhash)
+  end
+
+  # Use embedded fonts to detect the type of PDF
+  def detectPDFType
+    out = `pdffonts #{@input}`.split("\n")
+    if out.length > 4
+      return embedPDF
+    else
+      return ocrPDF
+    end
+  end
+
+  # Extract text from embedded text PDFs
+  def embedPDF
+    Docsplit.extract_text(@input, :ocr => false)
+    outfile = @input.split(".")
+    text = File.read(outfile[0]+".txt")
+    
+    # Clean up text and delete file
+    File.delete(outfile[0]+".txt")
+    cleanPDF(text)
+  end
+
+  # OCR PDFs and turn that text into a JSON
+  def ocrPDF
+    # Extract individual pages
+    Docsplit.extract_images(@input)
+    
+    # OCR
+    docs = Dir["*.png"]
+    Docsplit.extract_text(@input, :ocr => true, :output => 'text')
+    outfile = @input.split(".")
+    text = File.read("text/" + outfile[0] + ".txt")
+
+    # Clean up text and files
+    File.delete("text/" + outfile[0]+".txt")
+    Dir.delete("text")
+    docs.each do |d|
+      File.delete(d)
+    end
+    cleanPDF(text)
+  end
+
+  # Removes numbers from edges of legal documents
+  def cleanPDF(text)
+    text.gsub!(/\r?\n/, "\n")
+    text.each_line do |l|
+      lflag = 0
+      (1..28).each do |i|
+        if l == i.to_s+"\n"
+          lflag = 1
+        end
+      end
+
+      if lflag != 1 && l
+        @text += l
+      end
+    end
+    
+    return @text
+  end
+
+  # Extract PDF metadata
+  def extractMetadataPDF
+    @metadata = Hash.new
+    @metadata[:author] = Docsplit.extract_author(@input)
+    @metadata[:creator] =  Docsplit.extract_creator(@input)
+    @metadata[:producer] = Docsplit.extract_producer(@input)
+    @metadata[:title] = Docsplit.extract_title(@input)
+    @metadata[:subject] = Docsplit.extract_subject(@input)
+    @metadata[:date] = Docsplit.extract_date(@input)
+    @metadata[:keywords] = Docsplit.extract_keywords(@input)
+    @metadata[:length] = Docsplit.extract_length(@input)
+  end
+end
+
---
+++ uploadconvert.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'uploadconvert'
+  s.version     = '0.0.1'
+  s.date        = '2014-03-12'
+  s.summary     = "Collected conversion tools for Transparency Toolkit"
+  s.description = "Converts documents to the appropriate format for Transparency Toolkit."
+  s.authors     = ["M. C. McGrath"]
+  s.email       = 'shidash@shidash.com'
+  s.files       = Dir["lib/*.rb"]
+  s.homepage    =
+    'https://github.com/Shidash/UploadConvert'
+  s.license       = 'GPL'
+end
